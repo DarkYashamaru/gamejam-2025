304af25021a34977058669264158d0af2cc4ce5b
UnitySamerB <samer.buitrago@unity3d.com>
2025-01-26 00:43:37 -0500

    Changes to Sonar

--- Assets/_game/SonarLightAction.cs
+++ Assets/_game/SonarLightAction.cs
@@ -1,16 +1,43 @@
 using UnityEngine;
+using System.Collections;
 
 public class SonarLightAction : MonoBehaviour
 {
+    public Transform luz;
+    public GameObject head;
+    public float time;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    private void Awake()
     {
+        time = 5.0f;
+        luz = transform;
+        luz.transform.localPosition=head.transform.localPosition;
+        //luz.gameObject.SetActive(false);
+    }
+    private void Start()
+    {
+        StartSonarMoving(); 
+    }
+    public void StartSonarMoving()
+    {
+        StartCoroutine(SonarLightCoroutine());
+    }
+    IEnumerator SonarLightCoroutine()
+    {
+        yield return new WaitForSeconds(5);
+        Vector3 startingPos = transform.position;
+        Vector3 finalPos = transform.position - (transform.forward * 15);
+        luz.gameObject.SetActive(true);
+        float elapsedTime = 0;
+
+        while (elapsedTime < time)
+        {
+            transform.position = Vector3.Lerp(startingPos, finalPos, (elapsedTime / time));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
