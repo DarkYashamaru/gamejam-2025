64cb9f6252c264a41db63ac69b7ccc442b2819fc
Ricardo Barrios <ricardo.barrios@unity3d.com>
2025-01-26 04:35:33 -0500

    Changed UP orientation of FollowPlayerSonar script

--- Assets/_game/Scripts/FollowPlayerSonar.cs
+++ Assets/_game/Scripts/FollowPlayerSonar.cs
@@ -5,13 +5,12 @@ public class FollowPlayerSonar : MonoBehaviour
     public Transform player;
     public float yOffsetCamera;
 
-   
-
-
     private void LateUpdate()
     {
         Vector3 playerPos = player.position;
-        playerPos.y += yOffsetCamera;
-        transform.position = playerPos;
+        Vector3 playerUp = player.up;
+        Vector3 offset = playerUp * yOffsetCamera;
+        transform.position = playerPos + offset;
+        transform.up = playerUp;
     }
 }
